d188742eafe882624db55e8e998dda8d38b29b54
LuisCerecer <113267773+LuisCerecer@users.noreply.github.com>
2025-07-13 12:16:02 -0500

    Updated Servicios.tsx

--- src/pages/Servicios.tsx
+++ src/pages/Servicios.tsx
@@ -317,7 +317,7 @@ function Servicios() {
                 </div>
                 <div className="flex justify-center">
                   <img 
-                    src="https://images.pexels.com/photos/236705/pexels-photo-236705.jpeg?auto=compress&cs=tinysrgb&w=800" 
+                    src="https://res.cloudinary.com/dy089iwsg/image/upload/v1751399679/CYS_ianrni.jpg" 
                     alt="Reparación de Juntas" 
                     className="w-full h-auto object-cover rounded-lg shadow-md"
                   />
