c5760e0e11d9fc2382d671ac5f9c7a1c97c43541
LuisCerecer <113267773+LuisCerecer@users.noreply.github.com>
2025-07-16 20:40:56 -0500

    Updated Servicios.tsx

--- src/pages/Servicios.tsx
+++ src/pages/Servicios.tsx
@@ -273,7 +273,7 @@ function Servicios() {
                 </div>
                 <div className="flex justify-center">
                   <img 
-                    src="https://images.pexels.com/photos/1668928/pexels-photo-1668928.jpeg?auto=compress&cs=tinysrgb&w=800" 
+                    src="https://res.cloudinary.com/dy089iwsg/image/upload/v1752716410/Recu_mjyves.jpg" 
                     alt="Corrección de Planicidad por Recubrimientos" 
                     className="w-full h-auto object-cover rounded-lg shadow-md"
                   />
